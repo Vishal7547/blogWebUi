7440f194cd1e520757b552af69abd547a54d8f90
Vishal kumar <vishalk80269@gmail.com>
2023-04-15 15:47:32 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,30 @@
+let closed = document.getElementById("closed");
+let closedLogIn = document.getElementById("closedLogIn");
+let dark = document.getElementById("dark");
+function signIn() {
+  closed.style.display = "none";
+  closedLogIn.style.display = "block";
+  dark.srtyle
+}
+
+function joinGroup() {
+  closed.style.display = "block";
+}
+
+function CloserAccount() {
+  closed.style.display = "none";
+}
+
+function loginAccount() {
+  closedLogIn.style.display = "none";
+}
+
+function Login() {
+  closed.style.display = "block";
+  closedLogIn.style.display = "none";
+}
+
+function leaves() {
+  window.open("index.html");
+  //   alert("hello");
+}
